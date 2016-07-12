3908cb47413f9b17048ea536e38fc1eb65e06831
SpartanSOS <markb539@gmail.com>
2016-07-12 13:39:14 -0500

    code cleanup

--- client/components/RepoMap.js
+++ client/components/RepoMap.js
@@ -75,18 +75,3 @@ function renderGraph (state, vnode) {
     .datum( branchData )
     .call( state.chart )
 }
-
-// var commits = [
-//   {class: "pA", label: "person a", times: [
-//     {"starting_time": 1355752800000, "ending_time": 1355759900000},
-//     {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
-//   {class: "pB", label: "person b", times: [
-//     {"starting_time": 1355759910000, "ending_time": 1355761900000}]},
-//   {class: "pC", label: "person c", times: [
-//     {"starting_time": 1355761910000, "ending_time": 1355763910000}]}
-// ];
-//
-// var chart = d3.timeline();
-//
-// var svg = d3.select("#app").append("svg").attr("width", 500)
-//   .datum(commits).call(chart);
